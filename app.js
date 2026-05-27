67fdf6e9619577a4efceaa7a30155f73d718bf0d
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-05-27 11:33:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js CI/CD Pipeline POC 22!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
